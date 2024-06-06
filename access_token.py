74ca5b0bd91c7c0969675a69dd330b2bb8f39c58
Vasilii.shmyr <mr_tomat@mail.ru>
2024-06-06 13:01:43 +0300

    access token class changed to working one

--- access_token.py
+++ access_token.py
@@ -1,5 +1,5 @@
-import os, datetime
-from jose import jwt
+import os, datetime, common, requests, json
+from jwt import PyJWKClient, exceptions, decode
 import common, requests, json
 from vars.creds import password, client_id
 from vars.env_vars import token_url, username, token_file_path, keycloak_url, realm_name
@@ -16,50 +16,45 @@ class KeycloakTokenValidator:
         if os.path.isfile(self.token_file):
             with open(self.token_file, 'r') as file:
                 access_token = json.load(file)
-                log.debug("Getted token data: %s ", access_token)
-                isValid = self.validate_token(access_token)
-                if isValid:
+                log.debug("Fetched token data: %s", access_token)
+                if self.validate_token(access_token):
+                    log.info("Token validated and returned")
                     return access_token
                 else:
+                    log.info("Token invalid and requested new one")
                     return self.request_new_token()
         else:
+            log.info("Token file didn't finded. Requesting new token")
             return self.request_new_token()
 
     def validate_token(self, access_token):
-        validation_url = self.validation_url
-        headers = {"Authorization": f"Bearer {access_token}"}
         try:
-            response = common.s.get(validation_url, headers)
-            response.raise_for_status()
-            public_keys = response.json()
-        except requests.exceptions.HTTPError as e:
-            print(f"Ошибка при получении открытого ключа: {e}")
-            return False
-    
-        for key in public_keys['keys']:
-            try:
-                decoded_token = jwt.decode(access_token, key, algorithms=['RS256'])
-                log.debug("decode %s", decoded_token)
-                break
-            except jwt.exceptions.DecodeError
-        else:
-            print("Токен не прошел верификацию")
-            return False
+            jwk_client = PyJWKClient(self.validation_url)
+            signing_key = jwk_client.get_signing_key_from_jwt(access_token)
+            decoded_token = decode(access_token, signing_key.key, algorithms=["RS256"], options={"verify_signature": False})
+            log.debug("Decoded token: %s", decoded_token)
 
-        expires_at = decoded_token.get('exp')
-        if expires_at:
-            expires_at = datetime.datetime.fromtimestamp(expires_at)
-            if expires_at < datetime.datetime.now():
-                print("Токен истек")
-                return False
-    
-        return True
+            expires_at = decoded_token.get('exp')
+            if expires_at:
+                expires_at = datetime.datetime.fromtimestamp(expires_at)
+                if expires_at < datetime.datetime.now():
+                    log.error("Token has expired")
+                    return False
 
+            return True
+        except exceptions.ExpiredSignatureError as e:
+            log.error("Token has expired: %s", e)
+            return False
+        except exceptions.InvalidTokenError as e:
+            log.error("Invalid token error: %s", e)
+            return False
+        except Exception as e:
+            log.error("Error during token validation: %s", e)
+            return False
 
     def request_new_token(self):
         headers = {
             "Accept-Encoding": "gzip, deflate, br",
-            "response_type": "code",
             "Content-Type": "application/x-www-form-urlencoded"
         }
         token_data = {
@@ -69,20 +64,24 @@ class KeycloakTokenValidator:
             'password': password
         }
         try:
-            token_response = common.s.post(token_url, headers=headers, data=token_data)
-            token_response.raise_for_status() 
-            access_token = token_response.json()['access_token']
-            log.debug("new access token: %s", access_token)
+            token_response = requests.post(token_url, headers=headers, data=token_data)
+            token_response.raise_for_status()
+            access_token = token_response.json().get('access_token')
+            if not access_token:
+                log.error("No access token found in response")
+                return None
+            log.debug("New access token: %s", access_token)
             with open(self.token_file, 'w') as file:
                 json.dump(access_token, file)
             return access_token
         except requests.exceptions.HTTPError as e:
             log.error("HTTP exception: %s", e)
             log.error("Status code: %s", token_response.status_code)
-            log.error("Responce: %s", token_response.text)
+            log.error("Response: %s", token_response.text)
         except json.JSONDecodeError as e:
-            log.error("JSONDecodeError: %s", e)
-        return None
+            log.error("JSON decode error: %s", e)
+        return access_token
+
 
 
     def write_token(self, access_token):

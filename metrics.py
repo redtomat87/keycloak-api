a53169d2f99179dd7b5dc251101f12bcda88defe
Vasilii Shmyr <mr_tomat@mail.ru>
2025-03-27 14:50:16 +0300

    prometheus metrics added

---
+++ metrics.py
@@ -0,0 +1,18 @@
+from prometheus_client import Gauge
+
+CERT_EXPIRY_GAUGE = Gauge(
+    'keycloak_client_cert_expiry_seconds',
+    'Seconds until certificate expiration',
+    ['client_id', 'error']
+)
+
+CERT_VALID_GAUGE = Gauge(
+    'keycloak_client_cert_valid',
+    'Certificate validity status (1=valid, 0=invalid)',
+    ['client_id', 'error']
+)
+
+LAST_UPDATE_GAUGE = Gauge(
+    'keycloak_cert_check_last_update',
+    'Timestamp of last successful certificate check'
+)
